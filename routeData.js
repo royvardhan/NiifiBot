e68b0a8bdf5c1e33c30892b910697ff1a2254f2a
royvardhan <royvardhan.official@gmail.com>
2022-06-03 11:38:53 +0530

    Update routeData.js

--- routeData.js
+++ routeData.js
@@ -1,10 +1,10 @@
 import fetch from 'node-fetch';
 
-const ethNii = "https://api.niifi.com/api/v1/pools/0xb93D69BBeC0207E2234F93D6F8cC2A51A2BCAAB8"
-const wbtcNii = "https://api.niifi.com/api/v1/pools/0x310249c0AB6869e38Bf325Aada7fCc0258Ce2fea"
-const niiUsdc = "https://api.niifi.com/api/v1/pools/0xC4a405Fa62bd609B2fbE9a9Ab3a2961D060Cb760"
-const niiDai = "https://api.niifi.com/api/v1/pools/0xEaC657b647d079Aa4210FCc9156B8a6D4320d946"
-const niiNiifi = "https://api.niifi.com/api/v1/pools/0xd723993d38470f3B21Ae0aCB675b481c84420317"
+const ethNiiPool = "https://api.niifi.com/api/v1/pools/0xb93D69BBeC0207E2234F93D6F8cC2A51A2BCAAB8"
+const wbtcNiiPool = "https://api.niifi.com/api/v1/pools/0x310249c0AB6869e38Bf325Aada7fCc0258Ce2fea"
+const niiUsdcPool = "https://api.niifi.com/api/v1/pools/0xC4a405Fa62bd609B2fbE9a9Ab3a2961D060Cb760"
+const niiDaiPool = "https://api.niifi.com/api/v1/pools/0xEaC657b647d079Aa4210FCc9156B8a6D4320d946"
+const niiNiifiPool = "https://api.niifi.com/api/v1/pools/0xd723993d38470f3B21Ae0aCB675b481c84420317"
 
 let priceArr = []
 
@@ -26,7 +26,7 @@ const ethPriceInUSD = async () => {
 }
 
 const convertEthNii = async () => {
-    const res = await fetch(ethNii)
+    const res = await fetch(ethNiiPool)
     const data = await res.json()
     const price = data.data.token2.price
     const ethPrice = await ethPriceInUSD()
@@ -36,7 +36,7 @@ const convertEthNii = async () => {
 }
 
 const convertWbtcNii = async () => {
-    const res = await fetch(wbtcNii)
+    const res = await fetch(wbtcNiiPool)
     const data = await res.json()
     const price = data.data.token2.price
     const wbtcPrice = await wbtcPriceInUSD()
@@ -46,7 +46,7 @@ const convertWbtcNii = async () => {
 }
 
 const convertNiiUsdc = async () => {
-    const res = await fetch(niiUsdc)
+    const res = await fetch(niiUsdcPool)
     const data = await res.json()
     const niiPrice = data.data.token1.price
     priceArr.push(niiPrice)
@@ -54,18 +54,25 @@ const convertNiiUsdc = async () => {
 }
 
 const convertNiiDai = async () => {
-    const res = await fetch(niiDai)
+    const res = await fetch(niiDaiPool)
     const data = await res.json()
     const niiPrice = data.data.token1.price
     priceArr.push(niiPrice)
     console.log("Nii price in NIIDAI pool: " + niiPrice)
 }
 
-function updatePriceArray() {
-    convertEthNii()
-    convertWbtcNii()
-    convertNiiUsdc()
-    convertNiiDai()
+async function updatePriceArray() {
+   await convertEthNii()
+   await convertWbtcNii()
+   await convertNiiUsdc()
+   await convertNiiDai()
 }
 
-updatePriceArray()
+function getPercentage(i) {
+    let loopIndex
+    if (i === 0 ) {
+        loopIndex = 0
+    } else loopIndex = i -1
+    console.log(loopIndex)
+}
+
